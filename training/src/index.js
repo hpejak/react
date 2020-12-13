ff6f16c971a5c694d19d6640410cddd596c9e6a3
hpejak <0PAnT4oMiM@@>
2020-12-13 21:52:10 +0100

    React Training css added

--- training/src/index.js
+++ training/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function  Training(){
-    return <h1>This is start of Training</h1>;
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import './style.css'
+
+function Training() {
+    return (
+        <React.Fragment>
+            <div className='container'>
+                <PageTitle/>
+                <PageBody/>
+            </div>
+        </React.Fragment>
+    );
+}
+
+const PageTitle = () => {
+    return (
+        <h3 >This is start of Training</h3>
+    )
+}
+
+const PageBody = () => {
+    return (
+        <div>
+            <p>Training is in progress</p>
+        </div>
+    )
 }
 
 

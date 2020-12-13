d184e74869f1dd6b1143eeac6fef6d96e6aff677
Hrvoje <pejak.hrvoje@gmail.com>
2020-12-13 13:55:46 +0100

    Training react first add

---
+++ training/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function  Training(){
+    return <h1>This is start of Training</h1>;
+}
+
+
+ReactDom.render(<Training/>,document.getElementById('training'))

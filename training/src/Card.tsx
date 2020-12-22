fe3b5f5d1229174cd0f1aa1833c4f7f651678087
hpejak <0PAnT4oMiM@@>
2020-12-22 20:29:19 +0100

    Event Basics

--- training/src/Card.tsx
+++ training/src/Card.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const Card = ({name, data, id}) => {
+
+    const clickEvent = (entryName) => {
+        console.log("Some message " + entryName)
+    }
+
     return (
-        <div className="pageData">
+        <div className="pageData" onMouseOver={() => {console.log("This is " + id + " id")}}>
             <p>{id}</p>
             <p>{name}</p>
             <p>{data}</p>
-            </div>
+            <button type="button" onClick={() => clickEvent(name)}>Click Me</button>
+        </div>
     );
 }
 

c1e3c067f266b4796e336a1ba458ebc891ee0633
hpejak <0PAnT4oMiM@@>
2020-12-26 22:22:15 +0100

    useState basics

--- rudi/src/Rudi.tsx
+++ rudi/src/Rudi.tsx
@@ -1,11 +1,32 @@
 import Menu from './Menu'
+import {Button, Col, Container, Row} from "react-bootstrap";
+import {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Rudi = () => {
+
+    const [ rootName, setRootName ] = useState('Rudi');
+    let greeting = 'Hi, I am ' + rootName;
+
+    const onStart = () => {
+        rootName === 'Start' ? setRootName('Rudi') : setRootName('Start')
+    }
+
+
     return (
-        <div>
-            <h1>Hi I am Rudi</h1>
+        <Container fluid="xl">
+            <Row className="justify-content-xl-center" >
+                <Col xl={{span: 3, offset: 3}}>
+                    <h1>
+                        {greeting}
+                    </h1>
+                </Col>
+                <Col className="titleCol">
+                    <Button className="titleBtn" onClick={onStart}>Let's Start</Button>
+                </Col>
+            </Row>
             <Menu />
-        </div>
+        </Container>
     )
 }
 

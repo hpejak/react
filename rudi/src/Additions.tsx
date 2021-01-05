06f4f8259243243be8f7d763f9e40b09a948debe
hpejak <0PAnT4oMiM@@>
2021-01-05 21:54:59 +0100

    Add Item to the List

--- rudi/src/Additions.tsx
+++ rudi/src/Additions.tsx
@@ -3,25 +3,59 @@ import {useState} from "react";
 
 const Additions = () => {
 
-    const [monthName, setMonthName] = useState('');
+    const [date, setDate] = useState<string>('');
+    const [amount, setAmount] = useState<number>(0);
+    const [entries, setEntries] = useState<any[]>([]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Hello World');
+
+        if (date && amount > 0) {
+            const entry: object = {date, amountValue: amount};
+            setEntries((entries) => {
+                return [...entries, entry];
+            });
+
+            setDate('');
+            setAmount(0);
+        } else {
+            console.log("Please enter the fields in form");
+        }
     }
 
-    return (<div>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group as={Row} controlId="monthName">
-                <Form.Label column xl="3">Insert Month : </Form.Label>
-                <Col xl="3">
-                    <Form.Control type="text" value={monthName} onChange={(e) => setMonthName(e.target.value)} />
-                </Col>
-            </Form.Group>
-            <Button type="submit">Evaluate</Button>
-        </Form>
 
-    </div>)
+    return (
+        <div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group as={Row} controlId="date">
+                    <Form.Label column xl="3">Date : </Form.Label>
+                    <Col xl="4">
+                        <Form.Control type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="amount">
+                    <Form.Label column xl="3">Amount: </Form.Label>
+                    <Col xl="4">
+                        <Form.Control type="number" value={amount} onChange={(e) => {
+                            setAmount(e.target.value as any);
+                        }}/>
+                    </Col>
+                </Form.Group>
+                <Button type="submit">Evaluate</Button>
+            </Form>
+
+            {entries.map((entry,index) =>{
+                const {date, amountValue} = entry;
+                return (
+                  <div className="rowC entryDiv" key={index}>
+                      <h5 className="col-xl-6">{date}</h5>
+                      <p className="col-xl-3">{amountValue}</p>
+                  </div>
+                );
+            })}
+
+        </div>
+    );
 }
 
 export default Additions;

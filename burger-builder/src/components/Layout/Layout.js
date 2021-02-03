0f73cc420e0cbd673fc33cf20662bc45d0f5c9c8
Hrvoje Pejak <hrvoje.pejak@bulbtech.com>
2021-02-03 17:42:06 +0100

    burger builder init

---
+++ burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import Aux from '../../hoc/Auxiliary'
+
+const layout = (props) => {
+
+    return (
+        <Aux>
+            <div>
+                Toolbar, SideDrawer, Backdrop
+            </div>
+            <main>
+                {props.children}
+            </main>
+        </Aux>
+    )
+};
+
+export default layout;

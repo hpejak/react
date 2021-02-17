ca639efb443d7ca31e1b061625eeb59fdc8ff889
Hrvoje Pejak <hrvoje.pejak@bulbtech.com>
2021-02-17 15:31:50 +0100

    disabled fields in water

--- rudi/src/components/Water/WaterEditor/index.tsx
+++ rudi/src/components/Water/WaterEditor/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 //TODO Discrepancy consumption disabled. Double click enables it
 //TODO ALl Consumption Disabled. Calculated from state
 
-const WaterEditor = ({dbData, editChangeHandler,submitDataHandler}) => {
+const WaterEditor = ({dbData, editChangeHandler,submitDataHandler, enableEdit}) => {
 
     return (
         <>
@@ -14,22 +14,34 @@ const WaterEditor = ({dbData, editChangeHandler,submitDataHandler}) => {
                     <Row>
                         <Form.Group >
                             <Form.Label column="sm">Year</Form.Label>
-                            <Form.Control name="year" type="number" min="1900" max="2100" step="1" value={dbData.year || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="year" type="number" min="1900" max="2100" step="1"
+                                          value={dbData.year.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Month</Form.Label>
-                            <Form.Control name="month" type="number" min="1" max="12" step="1" value={dbData.month || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="month" type="number" min="1" max="12" step="1"
+                                          value={dbData.month.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group>
                             <Form.Label column="sm">Bill Payed</Form.Label>
-                            <Form.Control name="bill_payed_date" type="date" value={dbData.bill_payed_date || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="bill_payed_date"
+                                          type="date"
+                                          value={dbData.bill_payed_date.value || ''}
+                                          disabled={!dbData.bill_payed_date.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Bill Amount</Form.Label>
-                            <Form.Control name="bill_amount" type="number" value={dbData.bill_amount || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="bill_amount"
+                                          type="number"
+                                          value={dbData.bill_amount.value || ''}
+                                          disabled={!dbData.bill_amount.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
                     </Row>
                 </Col>
@@ -38,22 +50,38 @@ const WaterEditor = ({dbData, editChangeHandler,submitDataHandler}) => {
                     <Row>
                         <Form.Group >
                             <Form.Label column="sm">Reported m<sup>3</sup></Form.Label>
-                            <Form.Control name="consumption_reported" type="number" value={dbData.consumption_reported || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="consumption_reported"
+                                          type="number"
+                                          value={dbData.consumption_reported.value || ''}
+                                          disabled={!dbData.consumption_reported.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group>
                             <Form.Label column="sm">Read Date</Form.Label>
-                            <Form.Control name="read_date" type="date" value={dbData.read_date || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="read_date"
+                                          type="date"
+                                          value={dbData.read_date.value || ''}
+                                          disabled={!dbData.read_date.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Bill Consumption m<sup>3</sup></Form.Label>
-                            <Form.Control name="consumption_bill" type="number" value={dbData.consumption_bill || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="consumption_bill"
+                                          type="number"
+                                          value={dbData.consumption_bill.value || ''}
+                                          disabled={!dbData.consumption_bill.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Bill State</Form.Label>
-                            <Form.Control name="cons_state_bill" type="number" value={dbData.cons_state_bill || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="cons_state_bill"
+                                          type="number"
+                                          value={dbData.cons_state_bill.value || ''}
+                                          disabled={!dbData.cons_state_bill.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                     </Row>
@@ -65,22 +93,38 @@ const WaterEditor = ({dbData, editChangeHandler,submitDataHandler}) => {
                     <Row>
                         <Form.Group >
                             <Form.Label column="sm">First Flore Consumption m<sup>3</sup></Form.Label>
-                            <Form.Control name="consumption_first_flore" type="number" value={dbData.consumption_first_flore || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="consumption_first_flore"
+                                          type="number"
+                                          value={dbData.consumption_first_flore.value || ''}
+                                          disabled={!dbData.consumption_first_flore.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm" >Ground Flore Consumption m<sup>3</sup></Form.Label>
-                            <Form.Control name="consumption_ground_flore" type="number" value={dbData.consumption_ground_flore || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="consumption_ground_flore"
+                                          type="number"
+                                          value={dbData.consumption_ground_flore.value || ''}
+                                          disabled={!dbData.consumption_ground_flore.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Yard Flore Consumption m<sup>3</sup></Form.Label>
-                            <Form.Control name="consumption_yard_house" type="number" value={dbData.consumption_yard_house || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="consumption_yard_house"
+                                          type="number"
+                                          value={dbData.consumption_yard_house.value || ''}
+                                          disabled={!dbData.consumption_yard_house.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
-                        <Form.Group >
+                        <Form.Group onDoubleClick={enableEdit}>
                             <Form.Label column="sm">Discrepancy Consumption m<sup>3</sup></Form.Label>
-                            <Form.Control name="consumption_discrepancy" type="number" value={dbData.consumption_discrepancy || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="consumption_discrepancy"
+                                          type="number"
+                                          value={dbData.consumption_discrepancy.value || ''}
+                                          disabled={!dbData.consumption_discrepancy.edit}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                     </Row>
@@ -90,17 +134,23 @@ const WaterEditor = ({dbData, editChangeHandler,submitDataHandler}) => {
                     <Row>
                         <Form.Group >
                             <Form.Label column="sm">Amount External</Form.Label>
-                            <Form.Control name="amount_external" type="number" value={dbData.amount_external || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="amount_external" type="number"
+                                          value={dbData.amount_external.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Amount Communicated</Form.Label>
-                            <Form.Control name="amount_communicated" type="number" value={dbData.amount_communicated || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="amount_communicated" type="number"
+                                          value={dbData.amount_communicated.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">External Received</Form.Label>
-                            <Form.Control name="received_external" type="date" value={dbData.received_external || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="received_external" type="date"
+                                          value={dbData.received_external.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
                     </Row>
                 </Col>
@@ -111,22 +161,30 @@ const WaterEditor = ({dbData, editChangeHandler,submitDataHandler}) => {
                     <Row>
                         <Form.Group >
                             <Form.Label column="sm">First Flore m<sup>3</sup></Form.Label>
-                            <Form.Control name="cons_state_first_flore" type="number" value={dbData.cons_state_first_flore || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="cons_state_first_flore" type="number"
+                                          value={dbData.cons_state_first_flore.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Ground Flore m<sup>3</sup></Form.Label>
-                            <Form.Control name="cons_state_ground_flore" type="number" value={dbData.cons_state_ground_flore || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="cons_state_ground_flore" type="number"
+                                          value={dbData.cons_state_ground_flore.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Yard Flore m<sup>3</sup></Form.Label>
-                            <Form.Control name="cons_state_yard_house" type="number" value={dbData.cons_state_yard_house || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="cons_state_yard_house" type="number"
+                                          value={dbData.cons_state_yard_house.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Reported State m<sup>3</sup></Form.Label>
-                            <Form.Control name="cons_state_reported" type="number" value={dbData.cons_state_reported || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="cons_state_reported" type="number"
+                                          value={dbData.cons_state_reported.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                     </Row>
@@ -136,22 +194,30 @@ const WaterEditor = ({dbData, editChangeHandler,submitDataHandler}) => {
                     <Row>
                         <Form.Group >
                             <Form.Label column="sm">First Flore Amount</Form.Label>
-                            <Form.Control name="amount_first_flore" type="number" value={dbData.amount_first_flore || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="amount_first_flore" type="number"
+                                          value={dbData.amount_first_flore.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Ground Flore Amount</Form.Label>
-                            <Form.Control name="amount_ground_flore" type="number" value={dbData.amount_ground_flore || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="amount_ground_flore" type="number"
+                                          value={dbData.amount_ground_flore.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Yard Flore Amount</Form.Label>
-                            <Form.Control name="amount_yard_house" type="number" value={dbData.amount_yard_house || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="amount_yard_house" type="number"
+                                          value={dbData.amount_yard_house.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                         <Form.Group >
                             <Form.Label column="sm">Discrepancy Amount</Form.Label>
-                            <Form.Control name="amount_discrepancy" type="number" value={dbData.amount_discrepancy || ''} onChange={editChangeHandler}/>
+                            <Form.Control name="amount_discrepancy" type="number"
+                                          value={dbData.amount_discrepancy.value || ''}
+                                          onChange={editChangeHandler}/>
                         </Form.Group>
 
                     </Row>
--- rudi/src/components/Water/index.tsx
+++ rudi/src/components/Water/index.tsx
@@ -23,31 +23,30 @@ class Water extends Component{
         secondFlorePrice: 0,
         monthPrice: 0,
         dbData:{
-            year: this.nowYear.toString(),
-            month: this.nowMonth.toString(),
-            cons_state_reported: null,
-            cons_state_bill: null,
-            cons_state_first_flore: null,
-            cons_state_ground_flore: null,
-            cons_state_yard_house: null,
-            consumption_first_flore: null,
-            consumption_ground_flore: null,
-            consumption_yard_house: null,
-            consumption_all: null,
-            consumption_reported: null,
-            consumption_discrepancy: null,
-            consumption_bill: null,
-            bill_amount: null,
-            amount_first_flore: null,
-            amount_ground_flore: null,
-            amount_yard_house: null,
-            amount_discrepancy: null,
-            amount_external: null,
-            amount_communicated: null,
-            received_external: null,
-            read_date: null,
-            bill_payed_date: null,
-
+            year: {value: this.nowYear.toString()},
+            month: {value: this.nowMonth.toString()},
+            cons_state_reported: {value: null, edit: true},
+            cons_state_bill: {value: null, edit: true},
+            cons_state_first_flore: {value: null, edit: true},
+            cons_state_ground_flore: {value: null, edit: true},
+            cons_state_yard_house: {value: null, edit: true},
+            consumption_first_flore: {value: null, edit: true},
+            consumption_ground_flore: {value: null, edit: true},
+            consumption_yard_house: {value: null, edit: true},
+            consumption_all: {value: null, edit: true},
+            consumption_reported: {value: null, edit: false},
+            consumption_discrepancy: {value: null, edit: false},
+            consumption_bill: {value: null, edit: true},
+            bill_amount: {value: null, edit: true},
+            amount_first_flore: {value: null, edit: true},
+            amount_ground_flore: {value: null, edit: true},
+            amount_yard_house: {value: null, edit: true},
+            amount_discrepancy: {value: null, edit: true},
+            amount_external: {value: null, edit: true},
+            amount_communicated: {value: null, edit: true},
+            received_external: {value: null, edit: true},
+            read_date: {value: null, edit: true},
+            bill_payed_date: {value: null, edit: true}
         },
         oldData : {
             data: [],
@@ -86,11 +85,19 @@ class Water extends Component{
         const value = e.target.value;
 
         const dbDataEdited = this.state.dbData;
-        dbDataEdited[name] = value;
+        dbDataEdited[name].value = value;
 
         this.setState({
            dbData: dbDataEdited
         })
+    }
+
+    enableEdit = (e) => {
+        const name = e.target.name;
+        const dbDataState = this.state.dbData;
+
+        if (e.target.disabled) dbDataState[name].edit = true;
+        this.setState({dbData: dbDataState})
     }
 
     componentDidMount() {
@@ -109,45 +116,45 @@ class Water extends Component{
 
     submitDataHandler = () => {
 
-        const bill_payed: any = this.state.dbData.bill_payed_date;
+        const bill_payed: any = this.state.dbData.bill_payed_date.value;
         const bill_payed_string = bill_payed ? "'" + bill_payed.toString() + "'" : bill_payed;
 
-        const received_external: any = this.state.dbData.received_external;
+        const received_external: any = this.state.dbData.received_external.value;
         const received_external_string = received_external ? "'" + received_external.toString() + "'" : received_external;
 
-        const read_date: any = this.state.dbData.read_date;
+        const read_date: any = this.state.dbData.read_date.value;
         const read_date_string = read_date ? "'" + read_date.toString() + "'" : read_date;
 
-        let consumption_first_flore = this.state.dbData.consumption_first_flore;
-        let consumption_ground_flore = this.state.dbData.consumption_ground_flore;
-        let consumption_yard_house = this.state.dbData.consumption_yard_house;
+        let consumption_first_flore = this.state.dbData.consumption_first_flore.value;
+        let consumption_ground_flore = this.state.dbData.consumption_ground_flore.value;
+        let consumption_yard_house = this.state.dbData.consumption_yard_house.value;
         const consumption_all = (consumption_first_flore ? parseFloat(consumption_first_flore) : 0) +
             (consumption_ground_flore ? parseFloat(consumption_ground_flore): 0) + (consumption_yard_house ? parseFloat(consumption_yard_house): 0);
 
         const bodyInput = {
-            year: this.state.dbData.year,
-            month: this.state.dbData.month,
+            year: this.state.dbData.year.value,
+            month: this.state.dbData.month.value,
             bill_payed_date: bill_payed_string,
-            bill_amount: this.state.dbData.bill_amount,
-            cons_state_reported: this.state.dbData.cons_state_reported,
-            cons_state_bill: this.state.dbData.cons_state_bill,
+            bill_amount: this.state.dbData.bill_amount.value,
+            cons_state_reported: this.state.dbData.cons_state_reported.value,
+            cons_state_bill: this.state.dbData.cons_state_bill.value,
             consumption_first_flore: consumption_first_flore,
             consumption_ground_flore: consumption_ground_flore,
             consumption_yard_house: consumption_yard_house,
-            consumption_discrepancy: this.state.dbData.consumption_discrepancy,
-            consumption_bill: this.state.dbData.consumption_bill,
-            consumption_reported: this.state.dbData.consumption_reported,
+            consumption_discrepancy: this.state.dbData.consumption_discrepancy.value,
+            consumption_bill: this.state.dbData.consumption_bill.value,
+            consumption_reported: this.state.dbData.consumption_reported.value,
             consumption_all: consumption_all,
-            amount_external: this.state.dbData.amount_external,
-            amount_communicated: this.state.dbData.amount_communicated,
+            amount_external: this.state.dbData.amount_external.value,
+            amount_communicated: this.state.dbData.amount_communicated.value,
             received_external: received_external_string,
-            cons_state_first_flore: this.state.dbData.cons_state_first_flore,
-            cons_state_ground_flore: this.state.dbData.cons_state_ground_flore,
-            cons_state_yard_house: this.state.dbData.cons_state_yard_house,
-            amount_first_flore: this.state.dbData.amount_first_flore,
-            amount_ground_flore: this.state.dbData.amount_ground_flore,
-            amount_yard_house: this.state.dbData.amount_yard_house,
-            amount_discrepancy: this.state.dbData.amount_discrepancy,
+            cons_state_first_flore: this.state.dbData.cons_state_first_flore.value,
+            cons_state_ground_flore: this.state.dbData.cons_state_ground_flore.value,
+            cons_state_yard_house: this.state.dbData.cons_state_yard_house.value,
+            amount_first_flore: this.state.dbData.amount_first_flore.value,
+            amount_ground_flore: this.state.dbData.amount_ground_flore.value,
+            amount_yard_house: this.state.dbData.amount_yard_house.value,
+            amount_discrepancy: this.state.dbData.amount_discrepancy.value,
             read_date: read_date_string
         };
 
@@ -185,7 +192,7 @@ class Water extends Component{
                             consumptionLittleHouse={this.state.consumptionLittleHouse}
                 />
 
-                <WaterEditor dbData={this.state.dbData} editChangeHandler={this.editChangeHandler} submitDataHandler={this.submitDataHandler}/>
+                <WaterEditor dbData={this.state.dbData} editChangeHandler={this.editChangeHandler} submitDataHandler={this.submitDataHandler} enableEdit={this.enableEdit}/>
 
                 <WaterCalculation show={this.state.showInput}
                     monthPrice={this.state.monthPrice}
